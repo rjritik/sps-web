55e7ac142f5a2befd4403691bd7a87d74088498d
anilfoss <anilfoss@gmail.com>
2025-06-02 23:19:37 +0530

    change in app.js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-       <p>SPS WEB APP</p>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <header className="App-header">
+                <p>SPS WEB APP</p>
+                <p>Content goes here...</p>
+            </header>
+        </div>
+    );
 }
 
 export default App;
